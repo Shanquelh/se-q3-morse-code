2b2cfbcf0600f1bf75d5e60636db47736cb2b927
SHANQUEL H. SCOTT <shanquelh@yahoo.com>
2020-08-23 19:58:52 -0500

    All of my test passed in this assessment

--- morse.py
+++ morse.py
@@ -9,19 +9,32 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = 'Shanquel Scott working in group with Gaby, Sondos'
+# and study hall worked with John and Piero'
 
+import re
 from morse_dict import MORSE_2_ASCII
 
 
 def decode_bits(bits):
-    # your code here
-    return
+    bits = bits.strip('0')
+    sorting_nums = min([len(g) for g in re.findall(r"1+|0+", bits)])
+    bits_to_morse = bits.replace('0000000' * sorting_nums, '   ')
+    bits_to_morse = bits_to_morse.replace('000' * sorting_nums, ' ')
+    bits_to_morse = bits_to_morse.replace('111' * sorting_nums, '-')
+    bits_to_morse = bits_to_morse.replace('1' * sorting_nums, '.')
+    bits_to_morse = bits_to_morse.replace('0' * sorting_nums, '')
+    bits_to_morse = bits_to_morse.replace('0', '')
+    return bits_to_morse
 
 
 def decode_morse(morse):
-    # your code here
-    return
+    str1 = ''
+    for word in morse.strip().split("   "):
+        for char in word.strip().split(" "):
+            str1 += MORSE_2_ASCII[char]
+        str1 += " "
+    return str1.strip()
 
 
 if __name__ == '__main__':
@@ -35,5 +48,4 @@ if __name__ == '__main__':
     print()
     print("Part B:")
     print(f"'{hey_jude_bits}' -> {decode_morse(decode_bits(hey_jude_bits))}")
-
     print("\nCompleted.")
